be49e3d4e5c4b6433a6c920b2abc942156c28795
Jason Velasquez-Lopez <jasonv11@usf.edu>
2025-02-02 21:52:15 -0500

    Task 3- Inventory Depletion

--- cc_4.js
+++ cc_4.js
@@ -5,16 +5,25 @@ let actualAmount = purchaseAmount; // The actual price after discount
 if (purchaseAmount > 100 ) {
     actualAmount = purchaseAmount * 0.9
 } //Discount
-console.log('Final amount after discount: $${actualAmount}'); // Logging Amount after discount
+console.log(`Final amount after discount: $${actualAmount}`); // Logging Amount after discount
 
 
 //Task 2 For Loop
+
 let sales =[60, 40, 100, 70, 30]; //Array of sales figures
 let totalSales = 0 
 for (let i = 0; i < sales.length; i++) {
     totalSales += sales[i]
     
 }; //Loop to Calculate Total Sales
-console.log('Total Sales: $${totalSales}'); // Logging total sales
+console.log(`Total Sales: $${totalSales}`); // Logging total sales
 
+//Task 3 While Loop
 
+let stock = 10; //Declaring initial stock
+while (stock > 0) {
+    console.log(`Current Stock: ${stock}`);
+    stock--
+    
+}; // Decreasing stock until it reaches 0
+console.log("No Stock Left") // When no stock is left
