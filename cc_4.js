0267226f3cee44d120b6eac1a95037ac41ee04cb
Jason Velasquez-Lopez <jasonv11@usf.edu>
2025-02-02 21:14:39 -0500

    Task 1- Customer Discounts

---
+++ cc_4.js
@@ -0,0 +1,9 @@
+//Task 1 If Statements
+
+let purchaseAmount = 500; // Declaring a purchase amount
+let actualAmount = purchaseAmount; // The actual price after discount 
+if (purchaseAmount > 100 ) {
+    actualAmount = purchaseAmount * 0.9
+} //Discount
+console.log('Final amount after discount: $${actualAmount}'); // Logging Amount after discount
+
